35fe062ed9ae1e3646e6de3c7c2cc8d1053b49c2
Matti Luukkainen <mluukkai@iki.fi>
2016-01-09 14:43:27 +0200

    initial

---
+++ testi_spec.rb
@@ -0,0 +1,12 @@
+require './koodi.rb'
+
+RSpec.describe 'metodi kertoma' do
+  it "palauttaa parametrinsa kertoman" do
+    method_exists(self, :tulo, 1, 1)
+    expect(tulo(1, 1)).to eq(1)
+    expect(tulo(3, 2)).to eq(6)
+    expect(tulo(4, 5)).to eq(20)
+    expect(tulo(6, 10)).to eq(60)
+    expect(tulo(4, -2)).to eq(-8)
+  end
+end
