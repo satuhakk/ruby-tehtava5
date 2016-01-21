92d74737f87d810d9c11eb2b008f16b83e9266c8
Satu Häkkinen <s.j.hakkinen@gmail.com>
2016-01-21 19:43:36 +0200

    Taulukon syynäys toimii

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,12 @@
-# kirjoita koodi tänne
+def puuttuva(taulukko)
+  puuttuu = false
+  0.upto((taulukko.length)) do |i|
+    loytyyko = taulukko.include? i
+      if loytyyko == false
+        puuttuu = i
+      end
+  end
+  return puuttuu
+end
+
+puuttuva([0,1])
